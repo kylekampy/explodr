baa3af04f306167d7d4cf8b1321683ff8cb3f981
Kyle Kamperschroer <kyle@kylek.me>
2014-04-07 16:04:31 -0700

    First commit.

---
+++ js/ball.js
@@ -0,0 +1,88 @@
+/////////////////////////////////////////////////////////////////////////////
+// Ball class
+/////////////////////////////////////////////////////////////////////////////
+
+// The intent of this object is to be a ball. It will bounce like a ball, and
+// it will explode like a ball, like every other ball has done before it.
+
+// A ball has a number of attributes:
+// - Location x
+// - Location y
+// - Velocity x
+// - Velocity y
+// - Radius
+// - Color
+// - isExpanding
+// - isShrinking
+// - rateOfExpansion
+// - isDead
+
+// Default constructor
+function Ball(x, y, vX, vY,
+              r, clr, expanding,
+              shrinking, rateOfExpansion,
+              dead
+             ) {
+    this.x = x;
+    this.y = y;
+    this.velocityX = vX;
+    this.velocityY = vY;
+    this.radius = r;
+    this.color = clr;
+
+    // Some default values we need
+    this.isActioning = false;
+    this.isExpanding = false;
+    this.isExpanded = false;
+    this.framesTilDeath = 60;
+    this.isShrinking = false;
+    this.rateOfExpansion = 0;
+    this.isDead = false;
+
+    /////////////////////////////////////////////////////////////////////////////
+    // Functions
+    /////////////////////////////////////////////////////////////////////////////
+
+    // A function to explode this ball
+    this.explode = function(){
+        // Stop it from moving
+        this.velocityX = 0;
+        this.velocityY = 0;
+
+        // Set isExpanding to true
+        this.isActioning = true;
+        this.isExpanding = true;
+
+        // Set the rate of expansion to the default
+        this.rateOfExpansion = 3;
+    };
+
+    // A function to make this ball die
+    this.die = function(){
+        // Stop it from moving
+        this.velocityX = 0;
+        this.velocityY = 0;
+
+        // Set is shrinking to true
+        this.isActioning = true;
+        this.isShrinking = true;
+
+        // And we are no longer expanded
+        this.isExpanded = false;
+
+        // Set the rate of expansion to the default
+        this.rateOfExpansion = -0.05;
+    };
+
+    // A function to mark the ball as dead
+    this.markAsDead = function(){
+        this.isDead = true;
+        this.isExpanding = false;
+        this.isExpanded = false;
+        this.isShrinking = false;
+        this.isActioning = true; // leave as true since ball is not removed
+        this.radius = 0;
+    }
+
+}
+
---
+++ js/main.js
@@ -0,0 +1,375 @@
+/////////////////////////////////////////////////////////////////////////////
+// Explodr
+// (c) Kyle Kamperschroer, 2014
+/////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////
+// Members
+/////////////////////////////////////////////////////////////////////////////
+
+// If we are debugging or not
+var mDebug = true;
+
+// The canvas element
+var mCanvas = null;
+
+// The width of the canvas
+var mWidth = 0;
+
+// The height of the canvas
+var mHeight = 0;
+
+// The array of our ball objects
+var mBalls = [];
+
+// The array of possible color palettes
+var mPalettes = [];
+
+// The color palette
+var mPalette = null;
+
+// The current level
+var mLevel = 1;
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Functions
+/////////////////////////////////////////////////////////////////////////////
+
+// This function initializes all of our member variables
+function Init(){
+    // Get the canvas
+    mCanvas = $('#game');
+
+    // Get the width and height of the canvas
+    mWidth = mCanvas.width();
+    mHeight = mCanvas.height();
+
+    // Debug message
+    if (mDebug){
+        console.log("initialized members. Canvas = " + mCanvas +
+                    " has width = " + mWidth + " and height = " + mHeight);
+    }
+
+    // Build the color palettes
+    BuildPalettes();
+
+    // Now build the balls for the first level
+    BuildBalls(mLevel);
+
+    // Bind a mouse click event to the canvas
+    mCanvas.click(function(event){
+        RespondToClick(GetMousePosition(event));
+    });
+
+    // Setup animation frame stuff
+    if ( !window.requestAnimationFrame ) {
+        window.requestAnimationFrame = ( function() {
+            return window.webkitRequestAnimationFrame ||
+                window.mozRequestAnimationFrame ||
+                window.oRequestAnimationFrame ||
+                window.msRequestAnimationFrame ||
+                function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
+                    window.setTimeout( callback, 1000 / 60 );
+                };
+        } )();
+    }
+}
+
+// This function builds the color palettes variable
+function BuildPalettes(){
+    // These are all from the wonderful colour lovers website
+    
+    // Giant goldfishby manekineko
+    // http://www.colourlovers.com/palette/92095/Giant_Goldfish
+    mPalettes.push(
+        [
+            "#69D2E7",
+            "#A7DBD8",
+            "#E0E4CC",
+            "#F38630",
+            "#FA6900"
+        ]);
+
+    // Though Provoking by Miss_Anthropy
+    // http://www.colourlovers.com/palette/694737/Thought_Provoking
+    mPalettes.push(
+        [
+            "#ECD078",
+            "#D95B43",
+            "#C02942",
+            "#542437",
+            "#53777A"
+        ]);
+
+    // Cheer up emo kid by electrikmonk
+    // http://www.colourlovers.com/palette/1930/cheer_up_emo_kid
+    mPalettes.push(
+        [
+            "#556270",
+            "#4ECDC4",
+            "#C7F464",
+            "#FF6B6B",
+            "#C44D58"
+        ]);
+
+    // Ocean Five by DESIGNJUNKEE
+    // http://www.colourlovers.com/palette/1473/Ocean_Five
+    mPalettes.push(
+        [
+            "#00A0B0",
+            "#6A4A3C",
+            "#CC333F",
+            "#EB6841",
+            "#EDC951"
+        ]);
+
+    // Finally, set the current palette to one of these
+    mPalette = mPalettes[Math.floor(Math.random() * mPalettes.length)];
+}
+
+// The intent of this function is to build all of the balls for the
+// given level.
+function BuildBalls(level){
+    // The number of balls is equal to the level plus 5
+    var numBalls = level + 19;
+
+    // Build each ball
+    for (var i=0; i<numBalls; i++){
+        mBalls.push(
+            new Ball(
+                (Math.random() * (mWidth-10)) + 5, // x pos
+                (Math.random() * (mHeight-10)) + 5, // y pos
+                GenerateVelocity(), // velocity x
+                GenerateVelocity(), // velocity y
+                5, // radius
+                mPalette[Math.floor(Math.random() * mPalette.length)] // color
+                )
+            );
+    }
+
+    // Cool. Balls are built.
+}
+
+// This function will generate a random value between 5 and -5.
+// The absolute value will always be greater than 1
+function GenerateVelocity(){
+    var x = 0;
+    while(Math.abs(x) < 1){
+        x = (Math.random() * 5) - 2.5;
+    }
+    return x;
+}
+
+// This function updates everything
+function UpdateBalls(){
+    // Loop through each ball
+    $.each(mBalls, function(idx, ball){
+        if (ball.isDead){
+            // Nothing to do with a dead ball.
+        }else if (ball.isExpanding){
+            // Update the radius
+            ball.radius += ball.rateOfExpansion;
+
+            // Slow the rate of expansion by 10%
+            ball.rateOfExpansion *= 0.90;
+
+            // Check if the rate of expansion is low enough to stop
+            if (ball.rateOfExpansion < 0.05){
+                // Stop expanding
+                ball.rateOfExpansion = 0;
+
+                // Set the flag for isExpanded
+                ball.isExpanding = false;
+                ball.isExpanded = true;
+            }
+        }else if (ball.isExpanded){
+            // Check if it's time to die
+            if (ball.framesTilDeath <= 0){
+                ball.die();
+            }else{
+                // Subtract 1 from framesTilDeath
+                ball.framesTilDeath--;
+            }
+        }else if (ball.isShrinking){
+            // Update the radius
+            ball.radius += ball.rateOfExpansion;
+
+            // Accelerate the rate of expansion by 10%
+            ball.rateOfExpansion *= 1.10;
+
+            // Check if it's radius has dropped below zero
+            if (ball.radius <= 0){
+                // Wow, it's dead.
+                ball.markAsDead();
+            }
+        }else{ // The ball is moving! Collision checks
+            // Check if the ball has hit an 'x' wall
+            if (ball.x <= 2.5 ||
+                ball.x >= mWidth - 2.5){
+                
+                // Reverse the x velocity (bounce)
+                ball.velocityX = -ball.velocityX;
+            }
+
+            // Check if the ball has hit a 'y' wall
+            if (ball.y <= 2.5 ||
+                ball.y >= mHeight - 2.5){
+                
+                // Reverse the y velocity (bounce)
+                ball.velocityY = -ball.velocityY;
+            }
+
+            // Now check if this ball has hit any actionable ball
+            if (HasBallHitActioningBall(ball)){
+                ball.explode();
+            }else{
+                // Update the position
+                ball.x += ball.velocityX;
+                ball.y += ball.velocityY;
+            }
+        }
+    });
+}
+
+// This function updates the score
+function UpdateScoreboard(){
+    // Count the number of balls that aren't actioning
+    var count = 0;
+    for (var i=0; i<mBalls.length; i++){
+        var ball = mBalls[i];
+
+        // Check if not actioning
+        if (!ball.isActioning){
+            count++;
+        }
+    }
+
+    // Update the text in the scoreboard div
+    $('#balls_left').text(count);
+}
+
+// This function does collision checking to see if the provided
+// ball has colided with any other ball that is 'actioning'
+function HasBallHitActioningBall(ball){
+    // Iterate through
+    for (var i=0; i<mBalls.length; i++){
+        // Get the other ball
+        var otherBall = mBalls[i];
+
+        // Skip if it's dead
+        if (otherBall.isDead){
+            continue;
+        }
+
+        // See if it's actioning
+        if (otherBall.isExpanding ||
+            otherBall.isExpanded ||
+            otherBall.isShrinking){
+
+            // Now see if it's collided
+            var distX = ball.x - otherBall.x;
+            var distY = ball.y - otherBall.y;
+            var distance =
+                Math.abs(
+                    Math.sqrt(
+                        distX * distX + distY * distY
+                    )
+                )
+
+            // Check for collision
+            if (distance <= (ball.radius + otherBall.radius)){
+                return true;
+            }
+        }
+    }
+
+    // Nothing! Return false
+    return false;
+}
+
+// This function redraws everything
+function Draw(){
+    // Clear the canvas
+    var context = mCanvas[0].getContext('2d');
+    context.clearRect(0,0,mWidth,mHeight);
+    
+    // Draw each ball
+    $.each(mBalls, function(inx, ball){
+        DrawBall(context, ball);
+    });
+}
+
+// This function draws an individual ball
+function DrawBall(context, ball){
+    context.beginPath();
+    context.arc(Math.floor(ball.x),
+                Math.floor(ball.y),
+                ball.radius,
+                0,
+                2 * Math.PI,
+                false);
+    context.fillStyle = ball.color;
+    context.fill();
+    context.lineWidth = 1;
+    context.strokeStyle = ball.color;
+    context.stroke();
+}
+
+// This function is called once to start the game
+function Run(){
+    // Request an animation frame
+    requestAnimationFrame(Run);
+
+    // Update the balls
+    UpdateBalls();
+
+    // Update the scoreboard
+    UpdateScoreboard();
+
+    // Draw the scene
+    Draw();
+};
+
+// This function will get the click position
+function GetMousePosition(event){
+    var rect = mCanvas[0].getBoundingClientRect();
+    return {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top
+    };
+}
+
+// This function responds to a click from the user
+function RespondToClick(mousePos){
+    // Build a new ball to explode at this location
+    var explodingBall =
+        new Ball(
+            mousePos.x, // x pos
+            mousePos.y, // y pos
+            0, // x vel (not moving)
+            0, // y vel (not moving)
+            1, // radius
+            mPalette[Math.floor(Math.random() * mPalette.length)] // color
+        );
+
+    // Tell the ball to explode
+    explodingBall.explode();
+
+    // Push it to our array of balls
+    mBalls.push(explodingBall);
+
+    // That's it!
+}
+
+//// Execution Start ////
+$(function(){
+    // TODO -- Show loading spinner
+
+    // Init members
+    Init();
+
+    // Start the game loop
+    Run();
+    
+});
